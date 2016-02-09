37fa21ec517057bb71ddab084c6d59f4fdf4598b
Brookelyn <brooke.balza@gmail.com>
2016-02-09 15:16:05 +0000

    JSON data now pulling in correctly

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,6 @@ var potato = angular.module("potato",  ["ui.router"]);
 
 
 // Set-up
-
 potato.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 
@@ -12,44 +11,87 @@ potato.config(['$stateProvider', '$locationProvider', function($stateProvider, $
     templateUrl: '/templates/home.html'
   });
 
-  $stateProvider.state('testing', {
-    url: '/detail',
-    controller: 'mainPotato',
+  $stateProvider.state('detail', {
+    // : indicates that id is a variable that will be called in later
+    url: '/detail/:id',
+    controller: 'detailPotato',
     templateUrl: '/templates/detail.html'
   });
 
 }]);
 
-
-
 // Factory to get data
-
 potato.factory('PotatoPics', function($http) {
+  
+  var json;
+
+  function getJSON(callback) {
+    return $http.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK')
+    .success(function(data) {
+      json = data;
+      callback(data);
+    })
+    .error(function(data){
+      console.log('Error');
+    });
+  }
+
   return {
     getPics: function(callback) {
-      return $http.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK')
-      .success(function(data) {
-        callback(data);
-      })
-      .error(function(data){
-        console.log('Error');
-      });
+      if (json) {
+        return json;
+      } else {
+        getJSON(callback);
+      }
+    },
+    getDetail: function(id, callback) {
+      if (json) {
+        return callback(json.items[id]);
+      } else {
+        getJSON(function() {
+          return callback(json.items[id]);
+        });
+      }
     }
   };
 });
 
-
-
-
 // Homepage controller
-
-potato.controller('mainPotato', function($scope, $rootScope, PotatoPics) {
+potato.controller('mainPotato', function($scope, PotatoPics) {
   
   PotatoPics.getPics(function(data){
-    $rootScope.listings = data.items;
+    $scope.listings = data.items;
   });
-
-
+    
 });
 
+potato.controller('detailPotato', function($scope, PotatoPics, $stateParams) {
+  
+  PotatoPics.getDetail($stateParams.id, function(data){
+    $scope.listing = data;
+  });
+    
+});
 
+// Published date filter
+potato.filter('datePublished', function(listing) {
+  return function(published) {
+    console.log(published);
+  }
+})
+
+
+// filter('reverse', function() {
+//   return function(input, uppercase) {
+//     input = input || '';
+//     var out = "";
+//     for (var i = 0; i < input.length; i++) {
+//       out = input.charAt(i) + out;
+//     }
+//     // conditional based on optional argument
+//     if (uppercase) {
+//       out = out.toUpperCase();
+//     }
+//     return out;
+//   };
+// })
